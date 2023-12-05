3ffd64bd7ceb477f54e5518cdd0a77e9c5ee6443
laksamanaap <laksamana.arya1412@gmail.com>
2023-12-05 14:20:18 +0700

    feat : init commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h1 className="font-bold underline text-primary">Hello world!</h1>{" "}
+      {/* Example customize color*/}
+    </>
+  );
+}
+
+export default Home;
