ede06e410b33012c1552737ba0508cb2f3b5c835
sulthonaw <sultona412@gmail.com>
2023-12-05 16:45:03 +0700

    fix : missing file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
-import Home from "./components/pages/Home";
+import Home from "./pages/Home";
 
 function App() {
   return (
