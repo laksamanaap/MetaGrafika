780055b28063f9e3a1f4f491b447e6f834d87512
sulthonaw <sultona412@gmail.com>
2023-12-09 14:04:45 +0700

    feat : cta section

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,7 +1,37 @@
 import React from "react";
+import loket from "../assets/images/logo/loket.webp";
+import intersect from "../assets/images/intersect.webp";
 
 function Cta() {
-  return <div>Cta</div>;
+  return (
+    <section className="container px-4 lg:px-20 text-left">
+      <div className="relative bg-secondary rounded-2xl overflow-hidden grid gap-5 grid-cols-1 md:grid-cols-2  px-7 py-5 md:px-14 md:py-10">
+        <img src={intersect} alt="intersect.webp" className="absolute h-2/3 md:h-full -top-6 md:top-0 md:rotate-0 rotate-12 md:-left-52" />
+        <div className="text-white flex items-center">
+          <div>
+            <h1 className="font-semibold text-3xl md:text-5xl mb-2 md:mb-7">
+              Tiket Menuju
+              <br /> 3D Wisata Bahari
+            </h1>
+            <p className="text-slate-100">Dapatkan pengalaman menarik menjelajah wisata bahari dengan harga Rp. 20.000 secara digital melalui metaverse!</p>
+          </div>
+        </div>
+        <div className="flex items-center justify-center md:justify-end">
+          <div className="rounded-xl max-w-[25rem] bg-white p-5 w-full">
+            <div className=" rounded-xl aspect-video bg-slate-200 mb-2"></div>
+            <h2 className="font-semibold text-xl">Metaverse Wisata Bahari</h2>
+            <p className="text-slate-600 mb-2">4 - 30 Desember</p>
+            <button className="flex items-center justify-center w-full border border-b-2 border-primary rounded-lg py-2">
+              <div className="flex items-center gap-4">
+                <img src={loket} className="w-8 h-8 md:w-10 md:h-10" alt="loket.webp" />
+                <p className="font-medium text-sm md:text-base">Beli Sekarang</p>
+              </div>
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Cta;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Cta from "../components/Cta";
 
 function Home() {
   return (
     <>
       <h1 className="font-bold underline text-primary">Hello world!</h1>
       {/* Example customize color*/}
+      <Cta />
     </>
   );
 }
