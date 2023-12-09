1145d4b480100619bd0a985ff75c9d71d3552b5d
sulthonaw <sultona412@gmail.com>
2023-12-09 17:02:40 +0700

    feat : about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Cta from "../components/Cta";
+import About from "../components/About";
 
 function Home() {
   return (
     <>
       <h1 className="font-bold underline text-primary">Hello world!</h1>
       {/* Example customize color*/}
+      <About />
       <Cta />
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
       colors: {
         primary: "#0583D2", // Customize Color
         secondary: "#047392",
+        tertiary: "#C1F1FF",
+        accent: "#83E4FF",
+      },
+      container: {
+        center: true,
       },
     },
   },
