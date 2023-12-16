b6aa55ca3b6ea4382fbd4523be90deadb9f90072
laksamanaap <laksamana.arya1412@gmail.com>
2023-12-17 00:33:19 +0700

    feat : isometric component

--- src/components/elements/Isometric.jsx
+++ src/components/elements/Isometric.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { Suspense, useState, useEffect, useMemo } from "react";
+import Loading from "./Loading";
 
-const Isometric = () => {
-  return <div>Isometric</div>;
+// Import the component dynamically
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
+
+const Isometric = ({ splineUrl }) => {
+  const [cachedSpline, setCachedSpline] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const importSpline = async () => {
+      try {
+        const module = await import("@splinetool/react-spline");
+        setCachedSpline(module);
+        // optional
+        // setTimeout(() => {
+        //   setIsLoaded(true);
+        // }, 2000);
+      } catch (error) {
+        console.error("Error importing Spline component:", error);
+      }
+    };
+
+    importSpline();
+  }, []);
+
+  // Set Loading state
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      <Suspense fallback={<Loading />}>
+        {isLoaded && cachedSpline ? (
+          <cachedSpline.default
+            className="sm:h-720 sm:w-460 md:h-720 md:w-460 lg:h-720 lg:w-460"
+            scene={splineUrl}
+          />
+        ) : null}
+      </Suspense>
+      {!isLoaded && <Loading />}
+    </>
+  );
 };
 
 export default Isometric;
